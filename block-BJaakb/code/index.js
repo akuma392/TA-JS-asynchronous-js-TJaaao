f9c6be4f0bd65db7652f4eab0fcb684b5ac6f188
Abhishek Kumar <abhivision3@gmail.com>
2021-02-23 21:42:47 +0530

    refactor

--- block-BJaakb/code/index.js
+++ block-BJaakb/code/index.js
@@ -1,12 +1,17 @@
-function wait() {
-  return new Promise((resol, reject) => {
-    reject('failed');
-  });
-}
+// function wait() {
+//   return new Promise((resol, reject) => {
+//     reject(' promise failed');
+//   });
+// }
 
-try {
-  wait();
-} catch (error) {
-  console.log(error);
-  console.log('Promise failed');
-}
+// let time = wait()
+//   .catch((error) => console.log(error))
+//   .finally(() => console.log('promise settled'));
+
+var firstPromise, secondPromise;
+
+secondPromise = Promise.resolve('second');
+
+firstPromise = Promise.resolve(secondPromise);
+
+firstPromise.then((value) => console.log(value));
